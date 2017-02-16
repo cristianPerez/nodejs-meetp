6ea972eba46f2a6a1d5a56fa6d67fb9b5aa4132e
john Alexa Agudelo <john.agudelo@pragma.com.co>
2017-02-16 16:32:41 -0500

    adicion de babel en el servidor

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-var express = require('express');
-var app = express();
-
-var port = process.env.PORT || 3000
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000
 
 app.use(express.static('dist'));
 
-app.get('/api/getchats', function(req, res) {
+app.get('/api/getchats', (req, res) => {
     console.log("entro: " + req);
    res.json([{id: 1, message: 'Hola como estas'}, {id: 2, message: 'Muy bien'}, {id: 3, message: 'Y para cuando'}]);
 });
 
-
-app.listen(port, function(){
-    console.log('servidor en el puerto 3000');
-});
+app.listen(port, () => console.log('servidor en el puerto 3000'));
