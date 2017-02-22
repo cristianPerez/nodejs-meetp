3457e58c2f9cdf51e62b20cd279b85914356a098
Cristian Perez <cperez354@gmail.com>
2017-02-22 11:05:49 -0500

    last commit with boradcast in socket

--- server.js
+++ server.js
@@ -27,16 +27,15 @@ io.on('connection', (socket) =>{
 
     socket.on('req-message-gif', (msg)=> {
         console.log(msg);
-        request(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=&{msg.message}`, (error, response, body) =>{
-            var url_gif = "";
-            url_gif = JSON.parse(body).data.fixed_height_downsampled_url;
-            console.log(url_gif);
-            msg.gif = url_gif;
-            socket.emit('res-message-gif', msg);
+        request(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=&{msg.message}`, (error, response, body) => {
+            var url_gif = ""
+            url_gif = JSON.parse(body).data.fixed_height_downsampled_url
+            console.log(url_gif)
+            msg.gif = url_gif
+            socket.emit('res-message-gif', msg)
+            socket.broadcast.emit('res-message-gif', msg)
         })
     })
-
-
 })
 
 server.listen(port, () => console.log(`servidor en el puerto ${port}`))
